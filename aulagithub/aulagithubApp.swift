32ba4fdaf9fcd2cf4287499409679a2de51e552c
henhiga <henrihiga@gmail.com>
2023-06-29 16:18:19 -0300

    Initial Commit

---
+++ aulagithub/aulagithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  aulagithubApp.swift
+//  aulagithub
+//
+//  Created by Henrique on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct aulagithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
